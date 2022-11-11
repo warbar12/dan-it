cdea80abb6f64cbf7fba61a753380c9379d26ad4
Andrew <aselezen624@gmail.com>
2022-11-11 17:58:42 +0200

    done

--- task5.js
+++ task5.js
@@ -1,28 +1,51 @@
 /*
-1) Опишіть своїми словами, що таке метод об'єкту.
-2) Який тип даних може мати значення властивості об'єкта?
-3) Об'єкт це посилальний тип даних. Що означає це поняття?
+1)Опишіть своїми словами, що таке екранування, і навіщо воно потрібне в мовах програмування
+- для упрощения работы со строками, в стоку можно передавть часть кода JS или что угодно. так как 
+екранизировать мы можем разльчними способавми (\n, \b, \t ...) то это визуализирует вывод в читабельном формате.
 
-1) Это возможность упростить работу, если есть что-то, что может быть видоизменено и это часто использузуют,
-создают метод который волняет определенную логики. И при этом без добаления лишнего кода или действий.
-2) ключь и значения ключа, функцію,
-3) он служит для вызова метода, что-бы получить обьект и утавновить для него this.
-он нужен когда метод получается динамически с объекта, используя выражение.
+2)Які засоби оголошення функцій ви знаєте?
+- declaration, expression and arrow function.
+
+3) Що таке hoisting, як він працює для змінних та функцій?
+hoisting - объявление переменной или функции перемещается в начало кода, хотя в действительности этого не происходит,
+бьявления переменных и функций попадают в память на фазе компиляции, но остаются в коде на том месте.
+Одним из преимуществ помещения в память являеться возможность использовать функцию до ее объявления.
+Переменные могут быть инициализированы и использованы до их объявления но не могут быть использованы без инициализации.
 */
-
-
 function createNewUser(){
   
   const firstName = prompt("Add your name");
-  const lastName =  prompt("Add your surname");
+  const lastName = prompt("Add your surname");
+  const myBirthday = () =>{
+
+    let myDay = prompt("Add your birthday - dd.mm.yyyy");
+
+    while((!myDay || /[a-zа-яё]/i.test(myDay)) || myDay?.length < 7){
+      myDay = prompt("Add your birthday - dd.mm.yyyy");
+    }
+
+    // полученую дату розбиваем на массив 
+    const correctDate = myDay.split('.');
+     // получаем отдельно год
+    const correctDateYear = correctDate[2];
+    // получаем месяц(отсчет месяцев начинается с нуля 0 - январь, 10 - ноябрь) для валидности отнимаем 1
+    const correctDateMonth = parseInt (correctDate[1]) - 1; 
+     // получаем отдельно дату 
+    const correctDateDate =  parseInt (correctDate[0]);
+    // получаем валидную дату записуную в переменную 
+    const birthday = new Date (correctDateYear, correctDateMonth, correctDateDate);
+    // возвращаем дату в обьэкт 
+    return birthday
+  }
 
   const newUser = {
     _name :firstName,
     _secondName :lastName,
+    _birthday: myBirthday(),
 
   // запрет изменения на прямую имя 
     set name(value){
-      if(value?.length < 2) this._name = value;
+      if(value?.length < 0) this._name = value;
     },
     get name(){
       return this._name
@@ -30,13 +53,13 @@ function createNewUser(){
 
   // запрет изменения на прямую фамилию
     set secondName(value){
-      if(value?.length < 3) this._secondName = value;
+      if(value?.length < 0) this._secondName = value;
     },
     get secondName(){
       return this._secondName
     },
 
-    // доступ до изменения имени и фамилии
+    // access to change first and last name
     setFirstName(value){
       return this._name = value;
     },
@@ -44,11 +67,38 @@ function createNewUser(){
       return this._secondName = value;
     },
 
-  // возвращает значение в нижнем регистре 
+  // returns value in lowercase
     getLogin: function(){
       return (this._name[0] + this._secondName).toLowerCase()
+    },
+
+    // this method show your age right now, and considering upcoming birthday.
+    getAge(){
+      let day = new Date;
+      let myDate = new Date(this._birthday);
+      let age = day.getFullYear() - myDate.getFullYear();
+      if(day.getMonth() == myDate.getMonth()){
+        if(day.getDate() >= myDate.getDate()){
+          return age
+        }else{
+          return age = age -1
+        }
+      }
+      return age
+    },
+
+    // this method create your password
+    getPassword(){
+      let year = new Date(this._birthday)
+      return (this._name[0].toUpperCase() + this._secondName.toLowerCase() + year.getFullYear())
     }
   }
+
+  let yourAge = newUser.getAge();
+  console.log(yourAge)
+
+  let yourPasswod = newUser.getPassword();
+  console.log(yourPasswod);
 
   let showInfo = newUser.getLogin();
   console.log(showInfo)
@@ -56,4 +106,4 @@ function createNewUser(){
   return newUser;
 }
 
-console.log(createNewUser())
+console.log(createNewUser());
