cb5d6dacea5a92351b8cd5b3f8dd1f45b0905185
Andrew <aselezen624@gmail.com>
2022-12-01 15:02:45 +0200

    done

--- task8.js
+++ task8.js
@@ -1,24 +1,55 @@
-'use strict';
-const createdList = (arr, parent = document.body) =>{
+const e = ["Kharkiv", "Kiev", ["Borispol", "Irpin"], "Odessa", "Lviv", "Dnieper"];
 
-  arr.map(elemet => {
+const data = [["Регион города - 1", "Регион города - 2"], "Kharkiv", "Kiev", 
+["Borispol",["Село под городом - 1", "Село под городом - 2"], "Irpin"],
+"Odessa", "Lviv", "Dnieper"];
 
 
-  //   const list = `
-  //   <ul>
-  //   <li>${elemet}</li>
-  //   </ul>
-  //   `
-  //   parent.insertAdjacentHTML('beforeend', list)
-  })
+const getList = (arr, parent = document.body) => {
+  const mainList = document.createElement('ul');
+
+  const addItemToList = (arr, list) => arr.map((item, index) => {
+    const listItem = document.createElement('li');
+    const itemTitle = document.createElement('h3');
+
+    if(!!Array.isArray(item)){
+      const parrentItem = list.childNodes[index - 1];
+      const childrenList = document.createElement('ul');
+
+      addItemToList(item, childrenList)
+
+      if(!parrentItem){
+        itemTitle.innerText = 'City is not defined';
+        listItem.appendChild(itemTitle);
+        listItem.appendChild(childrenList);
+
+        return list.appendChild(listItem);
+      }
+
+      return parrentItem.appendChild(childrenList);
+    }
+    itemTitle.innerText = item;
+    listItem.appendChild(itemTitle);
+
+    return list.appendChild(listItem)
+  });
+
+  addItemToList(arr, mainList)
+
+  return parent.appendChild(mainList)
 }
-let q = ["hello","world", "Kiev", "Kharkiv", "Odessa", "Lviv"];
-let w = ["1", "2", "3", "sea", "user", 23];
-let e = ["Kharkiv", "Kiev", ["Borispol", "Irpin"], "Odessa", "Lviv", "Dnieper"]; 
-createdList(e)
+getList(data)
 
-/*
-<ul>
-  <li></li>
-</ul>
-*/ 
+// таймер обратного отсчета
+let t = 3;
+let time = document.getElementsByClassName("clock")[0];
+
+function fun() {
+  t--
+  time.innerHTML = t;
+  if(t < 0) clearInterval(inter);
+  if(t === 0) document.body.remove()
+}
+const inter = setInterval("fun()", 1000);
+fun()
+
