4cccc7601f235c7f9f8ebd1449e4ef5590e44608
Andrew <aselezen624@gmail.com>
2022-12-02 19:16:44 +0200

    done

--- tabs/tabs/task10.js
+++ tabs/tabs/task10.js
@@ -6,6 +6,39 @@
 При цьому потрібно, щоб функція, написана в джаваскрипті, через такі правки не переставала працювати.
 */
 
-const ul = document.querySelectorAll(".tabs-content")
+const firstUl = document.querySelector(".tabs");
+const secondUl = document.querySelector(".tabs-content")
+const buttonTab = [...firstUl.children];
+const showContent = [...secondUl.children]
 
-console.log(ul);
+showContent.forEach((item, i) => {
+  item.classList.add("tabs-content-info");
+  if(i === 0) item.classList.add("active");
+})
+
+// add "data-set" attributes for each element 
+buttonTab.forEach((item, index) => {
+  item.setAttribute("data-set",`${index}`)
+});
+
+showContent.forEach((item, index) => {
+  item.setAttribute("data-set",`${index}`)
+});
+
+
+firstUl.addEventListener('click', event => {
+  if(event.target !== firstUl){
+    buttonTab.forEach(item => {
+        item.classList.remove('active')
+      })
+    }
+    showContent.forEach(item => {
+      if(item.dataset.set === event.target.dataset.set){
+        item.classList.add("active")
+      }else{
+        item.classList.remove('active')
+      }
+    });
+    event.target.classList.toggle('active')
+})
+
